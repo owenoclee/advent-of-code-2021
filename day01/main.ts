890fe581eac25c5c5ed4dda740e54ef1fa25b452
Owen Oclee <owenoclee@users.noreply.github.com>
2021-12-01 22:54:32 +0000

    Day 1 part 2 solution

--- day01/main.ts
+++ day01/main.ts
@@ -1,14 +1,35 @@
-import { readLines } from "https://deno.land/std@0.116.0/io/buffer.ts";
+import { readAll } from "https://deno.land/std@0.116.0/streams/conversion.ts";
 
-let increases = 0;
-let prevN: number | undefined;
+const sonarSweep = (readings: number[], window: number) => {
+  let increases = 0;
+  let prev: number | undefined;
 
-for await (const l of readLines(Deno.stdin)) {
-  const n = Number(l);
-  if (prevN && n > prevN) {
-    increases++;
+  for (let i = 0; i < readings.length - (window - 1); i++) {
+    let sum = 0;
+    for (let wi = 0; wi < window; wi++) {
+      sum += readings[i + wi];
+    }
+    if (prev !== undefined && sum > prev) {
+      increases++;
+    }
+    prev = sum;
   }
-  prevN = n;
+
+  return increases;
+};
+
+const part = Deno.args[0];
+if (Deno.args.length != 1 || !/^(1|2)$/.test(part)) {
+  console.log("Part must be supplied (1 or 2)");
+  Deno.exit(1);
 }
 
-console.log(increases);
+const readings = new TextDecoder()
+  .decode(await readAll(Deno.stdin))
+  .split("\n")
+  .filter((l) => l !== "")
+  .map((l) => Number(l));
+
+const window = part === "1" ? 1 : 3;
+
+console.log(sonarSweep(readings, window));
